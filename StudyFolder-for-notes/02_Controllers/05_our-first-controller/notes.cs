63aa901a9b3bd05c966c94e80a3c1e3fed050b83
bxvail <BryantVail@gmail.com>
2019-01-26 16:38:04 -0500

    end 2-6

---
+++ StudyFolder-for-notes/02_Controllers/05_our-first-controller/notes.cs
@@ -0,0 +1,31 @@
+
+namespace Lesson2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World! I'm learning MVC!";
+        }
+    }
+}
+
+
+string ActionResults = new string[
+    actionResults,
+    EmptyResult,
+    FileResult,
+    HttpStatusCodeResult,
+    JavaScriptResult,
+    JSONResult,
+    RedirectResult,
+    RedirectToRoute
+];
+
+
+
+
+
+
+
+
